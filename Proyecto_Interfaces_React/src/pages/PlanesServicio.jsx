cf361cbd9d4bf4e4207834423fd28ce1223e249f
AdrianMP16 <adriananaluca2004@gmail.com>
2026-02-01 16:09:21 -0500

    Servicios V2 - Cambio en la logica

--- Proyecto_Interfaces_React/src/pages/PlanesServicio.jsx
+++ Proyecto_Interfaces_React/src/pages/PlanesServicio.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
 import { useNavigate } from "react-router-dom";
+import PlanCard from "../components/plancard/PlanCard.jsx";
 
 const PlanesServicio = () => {
   const navigate = useNavigate();
@@ -30,63 +31,41 @@ const PlanesServicio = () => {
 
         {/* Planes */}
         <div className="relative z-10 flex justify-center flex-wrap gap-[30px]">
-
-          {/* Plan Gratuito */}
-          <div className="bg-[#f8d77b] text-black w-[300px] rounded-[12px] p-[20px] shadow-[0_4px_10px_rgba(0,0,0,0.3)] transition-all duration-300 hover:-translate-y-1 hover:shadow-[0_8px_20px_rgba(0,0,0,0.5)]">
-            <h3 className="bg-[#243a63] text-white rounded-[8px] p-[10px]">
-              Plan FREE
-            </h3>
-            <ul className="text-left my-[15px]">
-              <li>Costo: $0.00</li>
-              <li>Usuario individual</li>
-              <li>Sincronización de un dispositivo</li>
-              <li>Recordatorios básicos</li>
-              <li>Incluye anuncios discretos</li>
-              <li>Colaboración no disponible</li>
-            </ul>
-            <button className="bg-[#243a63] text-white px-[20px] py-[10px] rounded-[10px] hover:bg-[#0e1a2a]">
-              Registrese
-            </button>
-          </div>
-
-          {/* Plan Mensual */}
-          <div className="bg-[#f8d77b] text-black w-[300px] rounded-[12px] p-[20px] shadow-[0_4px_10px_rgba(0,0,0,0.3)] transition-all duration-300 hover:-translate-y-1 hover:shadow-[0_8px_20px_rgba(0,0,0,0.5)]">
-            <h3 className="bg-[#243a63] text-white rounded-[8px] p-[10px]">
-              Plan Pro
-            </h3>
-            <ul className="text-left my-[15px]">
-              <li>Costo: $4.99</li>
-              <li>Sincronización multiplataforma</li>
-              <li>Recordatorios avanzados</li>
-              <li>5 GB en la nube</li>
-              <li>Colaboración disponible</li>
-              <li>Archivos ilimitados</li>
-              <li>Acceso por un mes</li>
-            </ul>
-            <button className="bg-[#243a63] text-white px-[20px] py-[10px] rounded-[10px] hover:bg-[#0e1a2a]">
-              Registrese
-            </button>
-          </div>
-
-          {/* Plan Anual */}
-          <div className="bg-[#f8d77b] text-black w-[300px] rounded-[12px] p-[20px] shadow-[0_4px_10px_rgba(0,0,0,0.3)] transition-all duration-300 hover:-translate-y-1 hover:shadow-[0_8px_20px_rgba(0,0,0,0.5)]">
-            <h3 className="bg-[#243a63] text-white rounded-[8px] p-[10px]">
-              Plan Ultra
-            </h3>
-            <ul className="text-left my-[15px]">
-              <li>Costo: $49.99</li>
-              <li>Funciones del plan Pro</li>
-              <li>Soporte prioritario</li>
-              <li>Acceso anticipado</li>
-              <li>Funciones de administrador</li>
-              <li>50 GB en la nube</li>
-              <li>Acceso por un año</li>
-            </ul>
-            <button className="bg-[#243a63] text-white px-[20px] py-[10px] rounded-[10px] hover:bg-[#0e1a2a]">
-              Registrese
-            </button>
-          </div>
-
+          <PlanCard
+            plan="FREE"
+            costo="$0.00"
+            beneficios={[
+              "Usuario individual",
+              "Sincronización de un dispositivo",
+              "Recordatorios básicos",
+              "Incluye anuncios discretos",
+              "Colaboración no disponible",
+            ]}
+          />
+          <PlanCard
+            plan="PRO"
+            costo="$4.99"
+            beneficios={[
+              "Sincronización multiplataforma",
+              "Recordatorios avanzados",
+              "5 GB en la nube",
+              "Colaboración disponible",
+              "Archivos ilimitados",
+              "Acceso por un mes",
+            ]}
+          />
+          <PlanCard
+            plan="ULTRA"
+            costo="$49.99"
+            beneficios={[
+              "Funciones del plan Pro",
+              "Soporte prioritario",
+              "Acceso anticipado",
+              "Funciones de administrador",
+              "50 GB en la nube",
+              "Acceso por un año",
+            ]}
+          />
         </div>
       </section>
 
