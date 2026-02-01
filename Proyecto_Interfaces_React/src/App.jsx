b1ca2b8ad6df37c773a2085944e920f7b355b916
AdrianMP16 <adriananaluca2004@gmail.com>
2026-02-01 10:12:13 -0500

    Funcional V2_Sin estilos

--- Proyecto_Interfaces_React/src/App.jsx
+++ Proyecto_Interfaces_React/src/App.jsx
@@ -8,8 +8,10 @@ import Register from "./pages/register/Register.jsx";
 import NotFound from "./pages/NotFound";
 import PlanesServicio from './pages/PlanesServicio.jsx';
 import Pomodoro from './pages/pomodoro/Pomodoro.jsx';
+import Dashboard from "./pages/dashboardUser/DashboardUser.jsx";
 
 import "./index.css"
+import ProtectedRoute from "./components/protectedRoute/ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -25,6 +27,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/planes-servicio" element={<PlanesServicio />} />
           <Route path="/pomodoro" element={<Pomodoro />} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="*" element={<NotFound/>}/>
 
         </Routes>
